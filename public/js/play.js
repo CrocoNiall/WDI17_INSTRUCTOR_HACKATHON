daa447fbe66f09e06638468b5492736299dded6f
Stephen Reid <steveyblam@hotmail.co.uk>
2016-02-15 12:06:56 +0000

    merged my work in to tom's

--- public/js/play.js
+++ public/js/play.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
   console.log("hello");
-  // test comment
-  // var socket = io();
+  
+  // user object TBC
+  var user = {
+  	"name":"steve"
+  }
 
-  // socket.on('connection', function() {
-  //   console.log('client has connected!');
-  // })
+  // set up the shake listener
+  var myShakeEvent = new Shake({
+      threshold: 15, // optional shake strength threshold
+      timeout: 1000 // optional, determines the frequency of event generation
+  });
+
+  // set up the socket connection
+  var socket = io();
+
+  // start listening for shakes
+  myShakeEvent.start();
+
+  // listen on the window too (just how it works)
+  window.addEventListener('shake', shakeEventDidOccur, false);
+
+  //function to call when shake occurs
+  function shakeEventDidOccur () {
+
+  	// message to server
+  	socket.emit('hit buzzer' , user);
+      
+  }
+  
 })
