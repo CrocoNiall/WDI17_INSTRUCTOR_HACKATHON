a048e29ecc4058e96dc423fa4bf26a17effa5264
Stephen Reid <steveyblam@hotmail.co.uk>
2016-02-15 11:39:36 +0000

    buzzer hit working in test files

--- app.js
+++ app.js
@@ -2,25 +2,39 @@ var express     = require('express');
 var app         = express();
 var port        = process.env.PORT || 3000;
 var router      = express.Router();
+var http        = require('http').Server(app);
 
 var routes     = require('./routes/routes');
 
-var bodyParser  = require('body-parser')
-var morgan      = require('morgan')
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('hit_buzzer' , function(user){
+
+  		console.log(user.name + " wants to answer");
+
+  })
+
+});
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
 app.use(morgan('dev'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }))
 
-app.use(router)
-app.use('/',  routes)
+app.use(router);
+app.use('/',  routes);
 
+app.use(express.static(__dirname + '/public'));
 
-
-
-
-
-
-app.listen(port)
+http.listen(port)
 console.log('Server started on port ' + port + '...')
--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,14 @@ var router = express.Router();
 // ***************SHOW ALL***************
 router.get('/', function(req,res){
  res.json('Hello Niall')
-})
+});
 
+router.get('/test' , function(req,res){
+
+	// test the accelerometer
+	res.render('test');
+
+});
 
 
 
